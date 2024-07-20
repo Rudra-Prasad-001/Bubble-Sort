5c91034f93356a028735abd2c28bbbd8bd7d8660
Rudra Prasad Pati <rudraprasadpati478@gmail.com>
2024-07-20 21:47:21 +0530

    Enhanced bubble sort by changing ineer for loop condition from jin 0..array.size-1 to j in 0..array.size-i-2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,11 +2,11 @@
 def bubble_sort(array)
     for i in 0..array.size-1 do
         swap = false #Using swap flags to terminate the loop when the array becomes sorted
-        for j in 1..array.size-1 do
-            if(array[j-1] > array[j]) then
-                temp = array[j-1]
-                array[j-1] = array[j]
-                array[j] = temp
+        for j in 0..array.size-i-2 do
+            if(array[j] > array[j+1]) then
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
                 swap = true
             end
         end
@@ -17,8 +17,7 @@ def bubble_sort(array)
     
 end
 
-bubble_sort([5,6,3,1,35,6,78,9,2])
-bubble_sort([34,4,12,56,98,5,33,42,2,41,92,33,211,334,446])
+bubble_sort([0,6,2,9,1,8]);
 
         
  
